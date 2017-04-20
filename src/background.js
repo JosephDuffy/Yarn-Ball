56d5408622ddb63a4b6ce20127dbfce83bf30576
Joseph Duffy <josephduffy@me.com>
2017-04-20 19:51:20 +0100

    Updated to ignore all textfield, textarea, script, and style elements

--- src/background.js
+++ src/background.js
@@ -2,8 +2,17 @@
 
 import Substituter from "./Substituter";
 
+const substituter = new Substituter();
+const ignoredElements = [
+    "TEXTFIELD", "TEXTAREA", "SCRIPT", "STYLE"
+]
+
 function applyFunctionToChildren(node, func) {
     for (let child of node.childNodes) {
+        if (ignoredElements.includes(node.nodeName)) {
+            continue;
+        }
+
         if (child.childNodes.length === 0) {
             func(child);
         } else {
@@ -12,21 +21,24 @@ function applyFunctionToChildren(node, func) {
     }
 };
 
-document.addEventListener("DOMContentLoaded", (event) => {
+function replaceCommandsInNode(node) {
+    if (ignoredElements.includes(node.nodeName)) {
+        return;
+    }
 
-    const substituter = new Substituter();
-
-    applyFunctionToChildren(document.body, (node) => {
-        if (node.nodeType === Node.ELEMENT_NODE) {
-            if (node.nodeName === "INPUT") {
-                if (node.type === "text" && node.readOnly) {
-                    node.value = substituter.replaceAllCommands(node.value);
-                }
-            } else if (node.nodeName !== "TEXTFIELD" && node.innerText) {
-                node.innerText = substituter.replaceAllCommands(node.innerText);
+    if (node.nodeType === Node.ELEMENT_NODE) {
+        if (node.nodeName === "INPUT") {
+            if (node.type === "text" && node.readOnly) {
+                node.value = substituter.replaceAllCommands(node.value);
             }
-        } else if (node.nodeType === Node.TEXT_NODE && node.parentNode.nodeName !== "TEXTAREA") {
-            node.nodeValue = substituter.replaceAllCommands(node.nodeValue);
+        } else if (node.innerText) {
+            node.innerText = substituter.replaceAllCommands(node.innerText);
         }
-    });
+    } else if (node.nodeType === Node.TEXT_NODE) {
+        node.nodeValue = substituter.replaceAllCommands(node.nodeValue);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    applyFunctionToChildren(document.body, replaceCommandsInNode);
 });
